78027027b424ca266d8b79f2e22f447862ec0391
Open-cobralibero <121136157+open-cobralibero@users.noreply.github.com>
2023-05-26 15:15:38 +0200

    Update skin.py

--- lib/python/skin.py
+++ lib/python/skin.py
@@ -15,7 +15,7 @@ from Tools.LoadPixmap import LoadPixmap
 
 MODULE_NAME = __name__.split(".")[-1].capitalize()
 
-DEFAULT_SKIN = SystemInfo["HasFullHDSkinSupport"] and "cobraliberosat.pli/skin.xml" or "PLi-HD/skin.xml"  # SD hardware is no longer supported by the default skin.
+DEFAULT_SKIN = SystemInfo["HasFullHDSkinSupport"] and "PLi-FullNightHD/skin.xml" or "PLi-HD/skin.xml"  # SD hardware is no longer supported by the default skin.
 EMERGENCY_SKIN = "skin_default/skin.xml"
 EMERGENCY_NAME = "Stone II"
 DEFAULT_DISPLAY_SKIN = "skin_default/skin_display.xml"
